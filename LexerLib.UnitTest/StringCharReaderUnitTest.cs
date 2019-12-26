2dcefdc4c9bcee08bcf3a7256852e38bd7c173fa
dfgs <dfgs83@gmail.com>
2019-12-26 10:42:23 +0100

    Added StringCharReader with unit tests

---
+++ LexerLib.UnitTest/StringCharReaderUnitTest.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using LexerLib.Exceptions;
+using LexerLib.Predicates;
+using LexerLib.UnitTest.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LexerLib.UnitTest
+{
+	[TestClass]
+	public class StringCharReaderUnitTest
+	{
+		[TestMethod]
+		public void ShouldHaveValidConstructor()
+		{
+			Assert.ThrowsException<ArgumentNullException>(() => new StringCharReader(null)); ;
+		}
+
+		[TestMethod]
+		public void ShouldPeekFirstCharacter()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual(0, reader.Position);
+			Assert.AreEqual('W', reader.Peek());
+			Assert.AreEqual(0, reader.Position);
+			Assert.AreEqual('W', reader.Peek());
+			Assert.AreEqual(0, reader.Position);
+		}
+
+		[TestMethod]
+		public void ShouldPopFirstCharacter()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual(0, reader.Position);
+			Assert.AreEqual('W', reader.Pop());
+			Assert.AreEqual(1, reader.Position);
+			Assert.AreEqual('e', reader.Peek());
+			Assert.AreEqual(1, reader.Position);
+		}
+
+		[TestMethod]
+		public void ShouldPopCharacters()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual(0, reader.Position);
+			Assert.AreEqual('W', reader.Pop());
+			Assert.AreEqual(1, reader.Position);
+			Assert.AreEqual('e', reader.Pop());
+			Assert.AreEqual(2, reader.Position);
+			Assert.AreEqual('l', reader.Pop());
+			Assert.AreEqual(3, reader.Position);
+			Assert.AreEqual('c', reader.Pop());
+			Assert.AreEqual(4, reader.Position);
+			Assert.AreEqual('o', reader.Pop());
+			Assert.AreEqual(5, reader.Position);
+			Assert.AreEqual('m', reader.Pop());
+			Assert.AreEqual(6, reader.Position);
+			Assert.AreEqual('e', reader.Pop());
+			Assert.AreEqual(7, reader.Position);
+		}
+		[TestMethod]
+		public void ShouldReturnEOF()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('W', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('e', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('l', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('c', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('o', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('m', reader.Pop());
+			Assert.IsFalse(reader.EOF);
+			Assert.AreEqual('e', reader.Pop());
+			Assert.IsTrue(reader.EOF);
+		}
+		[TestMethod]
+		public void ShouldPeekAndPop()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual('W', reader.Peek());
+			Assert.AreEqual('W', reader.Pop());
+			Assert.AreEqual('e', reader.Peek());
+			Assert.AreEqual('e', reader.Pop());
+			Assert.AreEqual('l', reader.Peek());
+			Assert.AreEqual('l', reader.Pop());
+			Assert.AreEqual('c', reader.Peek());
+			Assert.AreEqual('c', reader.Pop());
+			Assert.AreEqual('o', reader.Peek());
+			Assert.AreEqual('o', reader.Pop());
+			Assert.AreEqual('m', reader.Peek());
+			Assert.AreEqual('m', reader.Pop());
+			Assert.AreEqual('e', reader.Peek());
+			Assert.AreEqual('e', reader.Pop());
+		}
+
+		[TestMethod]
+		public void ShouldNotPopCharacterAtEOF()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual('W', reader.Pop());
+			Assert.AreEqual('e', reader.Pop());
+			Assert.AreEqual('l', reader.Pop());
+			Assert.AreEqual('c', reader.Pop());
+			Assert.AreEqual('o', reader.Pop());
+			Assert.AreEqual('m', reader.Pop());
+			Assert.AreEqual('e', reader.Pop());
+			Assert.ThrowsException<System.IO.EndOfStreamException>(() => reader.Pop());
+		}
+		[TestMethod]
+		public void ShouldNotPeekCharacterAtEOF()
+		{
+			ICharReader reader;
+
+			reader = new StringCharReader("Welcome");
+			Assert.AreEqual('W', reader.Pop());
+			Assert.AreEqual('e', reader.Pop());
+			Assert.AreEqual('l', reader.Pop());
+			Assert.AreEqual('c', reader.Pop());
+			Assert.AreEqual('o', reader.Pop());
+			Assert.AreEqual('m', reader.Pop());
+			Assert.AreEqual('e', reader.Pop());
+			Assert.ThrowsException<System.IO.EndOfStreamException>(() => reader.Peek());
+		}
+
+
+	}
+}
---
+++ LexerLib/StringCharReader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexerLib
+{
+	public class StringCharReader : ICharReader
+	{
+
+		private string value;
+		public bool EOF => position==value.Length;
+
+		private int position;
+		public long Position => position;
+
+
+		public StringCharReader(string Value)
+		{
+			if (Value== null) throw new ArgumentNullException("Value");
+			this.value = Value;
+		}
+
+		
+		public char Peek()
+		{
+			if (EOF) throw new EndOfStreamException();
+			return value[position];
+		}
+
+		public char Pop()
+		{
+			if (EOF) throw new EndOfStreamException();
+			return value[position++];
+		}
+
+	}
+}
